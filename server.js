9b68ea4b83986e8a5fc1e732a490362bdc816a4e
asmahri2-afk <asmahri2@gmail.com>
2026-06-08 14:17:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_PORT == 587,
+  secure: false, // MUST be false for 587
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
